7cf8dfcb36e9129b434e294ef6eabfbbaaa49c2c
Pbezerra-dev <herbetyp@gmail.com>
2020-05-12 00:18:22 -0300

    fix quality code flake8

--- libpythonpro/tests/test_usuarios.py
+++ libpythonpro/tests/test_usuarios.py
@@ -1,10 +1,12 @@
-from libpythonpro.spam.db import Sessao, Conexao
+from libpythonpro.spam.db import Conexao
 from libpythonpro.spam.modelos import Usuario
 import pytest
+
 
 @pytest.fixture
 def conexao():
     return Conexao()
+
 
 @pytest.fixture
 def sessao(conexao):
@@ -13,11 +15,13 @@ def sessao(conexao):
     sessao_obj.roll_back()
     sessao_obj.fechar()
 
+
 @pytest.fixture
 def conexao():
     conexao_obj = Conexao()
     yield conexao_obj
     conexao_obj.fechar()
+
 
 def test_salvar_usuario(sessao):
     usuario = Usuario(nome='Herbety')
@@ -25,10 +29,8 @@ def test_salvar_usuario(sessao):
     assert isinstance(usuario.id, int)
 
 
-
 def test_listar_usuarios(sessao):
     usuarios = [Usuario(nome='Herbety'), Usuario(nome='Paulo')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-
